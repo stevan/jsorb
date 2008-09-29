41bc288c7e54b0b32cba5b79a6494b0dbfbdda74
Stevan Little <stevan.little@iinteractive.com>
2008-09-29 12:45:24 +0000

    kioku navigagtor

--- example/kiokudb_navigator/jsorb_server.pl
+++ example/kiokudb_navigator/jsorb_server.pl
@@ -18,34 +18,72 @@ use KiokuDB::Backend::Serialize::JSPON::Collapser;
 {
     package Person;
     use Moose;
-    
+    use MooseX::AttributeHelpers;
+
     has ['first_name', 'last_name'] => (is => 'rw', isa => 'Str');
-    has 'age'    => (is => 'rw', isa => 'Int');
-    has 'spouse' => (is => 'rw', isa => 'Person');
+    has 'age'      => (is => 'rw', isa => 'Int');
+    has 'spouse'   => (is => 'rw', isa => 'Person');
+    
+    has ['mother', 'father'] => (
+        is        => 'ro', 
+        isa       => 'Person', 
+        weak_ref  => 1, 
+        trigger   => sub {
+            my ($self, $parent) = @_;
+            $parent->add_child($self);
+        }
+    );
+    
+    has 'children' => (
+        metaclass => 'Collection::Array',
+        is        => 'rw', 
+        isa       => 'ArrayRef[Person]',
+        default   => sub { [] },
+        provides  => {
+            'push' => 'add_child'
+        }
+    );
+
+    has 'car' => (is => 'rw', isa => 'Car');
+    
+    package Car;
+    use Moose;
+    
+    has 'owner' => (
+        is       => 'rw', 
+        isa      => 'Person', 
+        weak_ref => 1,
+        trigger  => sub {
+            my ($self, $owner) = @_;
+            $owner->car($self);
+        }
+    );    
+    
+    has [ 'make', 'model', 'vin' ] => (is => 'rw');
 }
 
-my $db = KiokuDB->connect("bdb:dir=data");
+my $db = KiokuDB->connect("bdb:dir=data", create => 1);
 
 my $s = $db->new_scope;
 
-my $me = Person->new(
-    first_name => 'Stevan',
-    last_name  => 'Little',
-    age        => 35,
-);
-
-my $lu = Person->new(
-    first_name => 'Lucinda',
-    last_name  => 'Juliani',
-    age        => 32,        
-    spouse     => $me,
-);
-
+my $me = Person->new(first_name => 'Stevan', last_name => 'Little', age => 35);
+my $lu = Person->new(first_name => 'Lucinda', last_name => 'Juliani', age => 32, spouse => $me);
 $me->spouse($lu);
 
+my $minivan = Car->new(make => 'Toyota', model => 'Sienna', vin => '12345abcdefghijklmno', owner => $lu);
+my $volvo   = Car->new(make => 'Volvo', model => 'Sedan', vin => '12345abcdefghijklmno', owner => $me);
+
+my %parents = (father => $me, mother => $lu);
+
+my @children = (
+    Person->new(first_name => 'Xoe',       last_name => 'Juliani', age => 11, %parents),
+    Person->new(first_name => 'Annabelle', last_name => 'Juliani', age => 6,  %parents),
+    Person->new(first_name => 'Benjamin',  last_name => 'Juliani', age => 6,  %parents),        
+);
+
 my ($me_id) = $db->txn_do(sub {
-    $db->store( $me, $lu );
-});    
+    $db->store( $me );
+});
 
 sub collapse_object {
     my $object = shift;
@@ -61,7 +99,7 @@ my $ns = JSORB::Namespace->new(
             procedures => [
                 JSORB::Procedure->new(
                     name  => 'lookup',
-                    body  => sub {  
+                    body  => sub {
                         my $id  = shift || $me_id;
                         my $obj = $db->lookup($id);
                         collapse_object($obj);
